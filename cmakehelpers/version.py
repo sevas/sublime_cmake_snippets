c35520630ef0d61d1afd918b0d26dada696b4c67
sevas <f.degroef@gmail.com>
2014-01-01 22:55:24 +0100

    made format strings py2.6-compatible

--- cmakehelpers/version.py
+++ cmakehelpers/version.py
@@ -3,11 +3,11 @@ VERSION_MINOR = 2
 VERSION_PATCH = 0
 VERSION_EXTRA = '-dev'
 
-__version__ = "{}.{}.{}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
-__loose_version__ = "{}.{}".format(VERSION_MAJOR, VERSION_MINOR)
+__version__ = "{0}.{1}.{2}".format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+__loose_version__ = "{0}.{1}".format(VERSION_MAJOR, VERSION_MINOR)
 
 if VERSION_EXTRA:
-    __version__ = "{}{}".format(__version__, VERSION_EXTRA)
+    __version__ = "{0}{1}".format(__version__, VERSION_EXTRA)
     __version_info__ = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, float('inf'))
 else:
     __version_info__ = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
